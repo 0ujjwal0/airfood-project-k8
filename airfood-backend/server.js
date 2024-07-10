063d42ed6bbd85cedd7871e97e3af0886cc91262
ujjwal <ujjwal.12100544@lpu.in>
2024-07-10 20:35:14 +0530

    added files

--- airfood-backend/server.js
+++ airfood-backend/server.js
@@ -4,11 +4,10 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT|| 3000;
 
 app.use(cors());
 
-// Endpoint to get the JSON data
 app.get('/api/meals', (req, res) => {
     const filePath = path.join(__dirname, 'data.json');
     fs.readFile(filePath, 'utf8', (err, data) => {
