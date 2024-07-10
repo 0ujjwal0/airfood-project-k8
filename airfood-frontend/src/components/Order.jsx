7f92d924909f253b100171aff3a812452cac57b1
ujjwal <ujjwal.12100544@lpu.in>
2024-07-10 21:36:55 +0530

    update

--- airfood-frontend/src/components/Order.jsx
+++ airfood-frontend/src/components/Order.jsx
@@ -15,7 +15,7 @@ function Order() {
   const [data, setData] = useState({ meals: [] });
 
   useEffect(() => {
-    axios.get('https://airfood-project.onrender.com')
+    axios.get('http://localhost:3000/api/meals')
       .then(response => setData(response.data))
       .catch(error => console.error('Error fetching data:', error));
   }, []);
